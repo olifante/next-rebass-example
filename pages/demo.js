88a3c7fb4c62327676134edb22324d201beda703
Tiago Henriques <trinosauro@gmail.com>
2016-11-26 22:07:45 +0100

    Convert to CSS in JS

--- pages/demo.js
+++ pages/demo.js
@@ -12,19 +12,34 @@ var data = require('pkginfo')
 
 data.components = Object.keys(Rebass).length
 
-const style = css({
-  fontFamily: '-apple-system, BlinkMacSystemFont, sans-serif',
-  color: '#111',
-  backgroundColor: '#fff',
-  lineHeight: 1.5,
-  margin: 0,
-})
+const style = css(
+  {
+    "html": {
+      "-webkit-text-size-adjust": "100%",
+    },
+    "body": {
+      "fontFamily": "-apple-system, BlinkMacSystemFont, sans-serif",
+      "color": "#111",
+      "backgroundColor": "#fff",
+      "lineHeight": "1.5",
+      "margin": "0",
+    },
+    "Button_hover": {
+      "boxShadow": "inset 0 0 0 999px rgba(0, 0, 0, .25)",
+    },
+    "Input_input_focus": {
+      "outline": "none",
+      "boxShadow": "0 0 0 2px #00749d",
+    },
+  }
+)
 
 export default () => (
   <div className={style}>
     <Head>
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
     </Head>
     <App {...data} />
-</div>
+  </div>
 )
