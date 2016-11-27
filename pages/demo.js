244e3f5729ed2dbf98a3058a24e6eb806abce708
Tiago Henriques <trinosauro@gmail.com>
2016-11-27 16:00:33 +0100

    inline global css

--- pages/demo.js
+++ pages/demo.js
@@ -12,35 +12,31 @@ var data = require('pkginfo')
 
 data.components = Object.keys(Rebass).length
 
-const style = css(
-  {
-    html: {
-      "-webkit-text-size-adjust": "100%",
-    },
-    body: {
-      fontFamily: "-apple-system, BlinkMacSystemFont, sans-serif",
-      color: "#111",
-      backgroundColor: "#fff",
-      lineHeight: 1.5,
-      margin: 0,
-    },
-    ".Button:hover": {
-      boxShadow: "inset 0 0 0 999px rgba(0, 0, 0, .25)",
-    },
-    ".Input": {
-      "input:focus": {
-        outline: "none",
-        boxShadow: "0 0 0 2px #00749d",
-      }
-    }
-  }
-)
-
 export default () => (
-  <div className={style}>
+  <div>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <style>{`
+        html {
+            -webkit-text-size-adjust: 100%
+        }
+
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+            line-height: 1.5;
+            margin: 0;
+        }
+
+        .Button:hover {
+            box-shadow: inset 0 0 0 999px rgba(0, 0, 0, .25);
+        }
+
+        .Input input:focus {
+            outline: none;
+            box-shadow: 0 0 0 2px #00749d;
+        }
+    `}</style>
     </Head>
     <App {...data} />
   </div>
