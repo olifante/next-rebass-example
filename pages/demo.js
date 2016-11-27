1b159cf70b6cfc538fce903613323de3ea65002c
Tiago Henriques <trinosauro@gmail.com>
2016-11-27 16:03:28 +0100

    Take literal css out of JSX

--- pages/demo.js
+++ pages/demo.js
@@ -12,31 +12,33 @@ var data = require('pkginfo')
 
 data.components = Object.keys(Rebass).length
 
+const style = `
+html {
+    -webkit-text-size-adjust: 100%
+}
+
+body {
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    line-height: 1.5;
+    margin: 0;
+}
+
+.Button:hover {
+    box-shadow: inset 0 0 0 999px rgba(0, 0, 0, .25);
+}
+
+.Input input:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px #00749d;
+}
+`
+
 export default () => (
   <div>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <style>{`
-        html {
-            -webkit-text-size-adjust: 100%
-        }
-
-        body {
-            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-            line-height: 1.5;
-            margin: 0;
-        }
-
-        .Button:hover {
-            box-shadow: inset 0 0 0 999px rgba(0, 0, 0, .25);
-        }
-
-        .Input input:focus {
-            outline: none;
-            box-shadow: 0 0 0 2px #00749d;
-        }
-    `}</style>
+      <style>{style}</style>
     </Head>
     <App {...data} />
   </div>
